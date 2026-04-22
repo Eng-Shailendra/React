4ba3df4cea415b7cc11bb7c5822bb4756ebaa3d3
shailendra sahu <shailendra2130@gmail.com>
2026-04-22 13:16:58 +0530

    commit all

---
+++ Myapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+//! Component
+//? 1) class based Component
+//? 2) function bsed Component
+
+//! 2) function bsed Component --- a normal plane java script function which returns some piece of jsx is known a function based componet.
+
+const App = () => {
+  return (
+    <>
+      <h1>React learnig</h1>
+    </>
+  );
+};
+
+
+
+export default App;
